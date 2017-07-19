6fbf80e43dbbbb157092b1e8d7d0ab93c9c74b6c
Ian McCracken <ian.mccracken@gmail.com>
2017-07-19 09:31:03 -0500

    Add a facility to create symlinks within the container through an environment variable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
 USER_ID=${LOCAL_USER_ID:-9001}
+
+WORKDIR=${PWD}
+for f in ${SYMLINKS}; do
+	LINK=(${f//:/ })
+	mkdir -p $(dirname ${LINK[1]})
+	rm -rf ${LINK[1]}
+	ln -s ${LINK[0]} ${LINK[1]}
+done
+cd ${WORKDIR}
+
 adduser -D -s /bin/bash -u $USER_ID -g "" user
 export HOME=/home/user
 exec /sbin/su-exec user "$@"
