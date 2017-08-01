f793ea552d0e022745288c7b61e9f3ec30de91ba
Ian McCracken <ian.mccracken@gmail.com>
2017-08-01 14:36:39 -0500

    Upgrade to Go 1.9rc1. Include Docker in the image and allow access if mounted.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,5 +12,15 @@ done
 cd ${WORKDIR}
 
 adduser -D -s /bin/bash -u $USER_ID -g "" user 2>/dev/null
+
+# Ensure that the user has access to Docker, if it's mounted
+if [ -S /var/run/docker.sock ]; then
+  GID=$(stat -c %g /var/run/docker.sock)
+  if ! getent group $GID >/dev/null; then
+    addgroup -g $GID docker
+  fi
+  adduser user $(getent group $GID | sed 's/:.*//')
+fi
+
 export HOME=/home/user
 exec /sbin/su-exec user "$@"
